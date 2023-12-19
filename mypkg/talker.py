ee962542a985634da92e5d318c90d04bbe39b75a
Issei Kimura <kimura@hogehoge.com>
2023-12-19 17:08:24 +0900

    add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -8,30 +8,11 @@ class Talker():
         self.n = 0
         nh.create_timer(0.5, self.cb)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def cb():          #17行目で定期実行されるコールバック関数
-    global n       
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n#msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    n += 1
-=======
-def cb(request, response):          
-    if request.name == "木村一星":
-        response.age = 20
-    else:
-        response.age = 255
-
-    return response
->>>>>>> dev
-=======
     def cb(self):
         msg = Int16()
         msg.data = talker.n
         self.pub.publish(msg)
         self.n += 1
->>>>>>> dev
 
 rclpy.init()
 node = Node("talker")
